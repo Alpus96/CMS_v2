8a19d881174ca2de2fb4cf1daec7097e8518dd61
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-06-12 09:50:38 +0200

    Added content model

---
+++ app/models/Content_Model.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     *  TODO:
+     */
+    class Content_Model extends MySQL_Socket
+    {
+
+        function __construct()
+        {
+            parent::__construct();
+        }
+    }
+
+?>
