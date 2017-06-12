cee63541b247a08ff528578e698f21855234339c
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-06-12 09:50:21 +0200

    Added owner model

--- app/models/Owner_Model.php
+++ app/models/Owner_Model.php
@@ -5,30 +5,33 @@
     class Owner_Model extends MySQL_Socket
     {
 
-        protected function __construct($key)
+        protected function __construct()
+        { parent::__construct(); }
+
+        protected function getAllUsers ()
         {
-            //  TODO: Authenticate the given key.
+            //  TODO: Get and return all usernames in the database.
         }
 
-        protected function createUser ($username, $password)
+        protected function createUser ($username, $password, $type)
         {
             //  TODO:   Add a new user to the database.
         }
 
-        protected function changePW ($username, $newPassword)
+        protected function toggleLockedUser ($target)
         {
-            //  TODO:   Change the password of the target user in the database.
+            //  TODO: Lock or unlock target depending on previus state.
         }
 
-        protected function changeUsername ($username, $newUsername)
+        protected function changeUserType ($target, $type)
         {
-            //  TODO:   Change the username of the target user in the database.
+            //  TODO: Update the type field of target.
         }
 
-        protected function deleteUser ($username)
+        protected function deleteUser ($target)
         {
             //  TODO:   Remove the target user from the database.
         }
-        
+
     }
 ?>
