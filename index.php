70e2db9bf51295b6fbd5e808c02024a1cb4a9210
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-09-18 17:30:17 +0200

    Removed test code and edited comment

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-    //  NOTE:   '/projects/CMS_v2' should be repalced with '/CMS' before integration.
+    /*
+    *   NOTE:   '/projects/CMS_v2' should be repalced
+    *           with '/CMS' before integration.
+    * */
     $url = str_replace('/projects/CMS_v2', '', $_SERVER['REQUEST_URI']);
     require_once 'app/controllers/routes.php';
     $Router = new Router();
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') { $Router->post($url); }
     else if ($_SERVER['REQUEST_METHOD'] === 'GET') { $Router->get($url); }
-
-    $tst = (object)['type' => 1, 'username' => 'Alpus96', 'hash' => 'password'];
-    //print_r(array_keys((array)$tst));
-    print_r(array_keys((array)$tst));
-    echo array_keys((array)$tst) === ['type', 'username', 'hash'] ? 'true' : 'false' ;
  ?>
