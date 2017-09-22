4a35f0d77001a914e73f491bd5007147feae2210
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-09-22 11:21:50 +0200

    Added models for content types

---
+++ app/models/content/imagepost_model.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+     *  TODO:
+     */
+    class ImagePost_Model extends MySQL_Socket
+    {
+
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+        function create ()
+        {
+
+        }
+
+        function read ()
+        {
+
+        }
+
+        function update ()
+        {
+
+        }
+
+        function delete ()
+        {
+
+        }
+
+    }
+?>
---
+++ app/models/content/post_model.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     *  TODO:
+     */
+    class Post_Model extends MySQL_Socket
+    {
+
+        function __construct()
+        {
+            parent::__construct();
+            //  TODO: Confirm that the user is authorized.
+            //  NOTE: Or should it be done in routes?
+        }
+
+        function create ()
+        {
+
+        }
+
+        function read ()
+        {
+
+        }
+
+        function update ()
+        {
+
+        }
+
+        function delete ()
+        {
+
+        }
+        
+    }
+?>
