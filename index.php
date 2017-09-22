706b628d2d7028ea9ef82b1c4c038f75120cd7b2
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-09-22 10:49:19 +0200

    Changed name of router to routes

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     * */
     $url = str_replace('/projects/CMS_v2', '', $_SERVER['REQUEST_URI']);
     require_once 'app/controllers/routes.php';
-    $Router = new Router();
+    $routes = new Routes();
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') { $Router->post($url); }
-    else if ($_SERVER['REQUEST_METHOD'] === 'GET') { $Router->get($url); }
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') { $routes->get($url); }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST') { $routes->post($url); }
  ?>
