cff300d097b59f3b9b989cee33b1fcc56c795c58
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-09-28 16:14:33 +0200

    Corrected errors in active user class and implemented token class

--- app/library/socket/activeUser_socket.php
+++ app/library/socket/activeUser_socket.php
@@ -1,5 +1,6 @@
 <?php
-    /**
+    /*
+    *   TODO:   Review comments.
     *   TODO:   Add SQL-injection reg-ex.
     * */
     class activeUser_socket extends MySQL_socket
@@ -25,7 +26,7 @@
 
 			//	Initiate the logger class to log errors for debuging purposes.
             self::$logger = new logger();
-            self::$logName = '_user_socket_log';
+            self::$logName = '_activeUserSocket_log';
 
             //  Check if a key was passed as an argument.
             if (!$key)
@@ -36,7 +37,7 @@
     			$jwt_config = $json_socket->read('JWT_config');
 
                 //  Confirm the configuration key was successfully read.
-                if ($jwt_config && key($jwt_config) == 'key')
+                if ($jwt_config && key($jwt_config) == 'key' && $jwt_config->key != '')
                 {
                     //  If the key was read save it to be used for token encryption.
                     self::$key = $jwt_config->key;
@@ -87,7 +88,7 @@
 		}
 
 		/*
-		*	@description    The create function adds a timestap to an object
+		*	@description    The create function adds a timestamp to an object
         *                   and encodes it to a JWT.
 		*
 		*	@arguments      $object is the object with data
@@ -102,7 +103,7 @@
 
             //  Confirm the object contains the property 'username'.
 			if (!is_object($object) || !property_exists($object, 'username') ||
-                !property_exists($object, 'password'))
+                !property_exists($object, 'hash'))
 			{
                 //  If the passed argument was not an object or
                 //  the required property was not present
@@ -111,25 +112,13 @@
 				return false;
 			}
 
-            //  Confirm the username and password is registered in the database.
-            if (!$this->confirmUser($object->username, $object->password))
-            {
-                //  If not log the error and return false.
-                $this->error = 'WARNING: Suspected atempt to create token for non-user: '.$this->error;
-                self::$logger->log(
-                    self::$logName,
-                    $this->error
-                );
-                return false;
-            }
-
             //  Confirm that the user is not already active.
             if ($this->confirmInactive($object->username))
             {
                 //  If the user is not currently active add a timestamp
                 //  to the object before encoding it.
-    			$object->timestamp = time();
-    			$token = $this->JWT->encode($object, self::$key);
+    			//$object->timestamp = time();
+    			$token_string = $this->JWT->encode($object, self::$key);
 
                 //  Connect to the database.
     			$mysql = parent::connect();
@@ -142,7 +131,11 @@
                         'INSERT INTO ACTIVE_USERS SET USERNAME = ?, TOKEN = ?'))
     				{
                         //  Bind the parameters and perform the query.
-    					$query->bind_param('ss', $object->username, $token);
+    					$query->bind_param(
+                            'ss',
+                            $object->username,
+                            $token_string
+                        );
     					$query->execute();
 
                         //  Close the query and the connection.
@@ -158,7 +151,7 @@
                             //  If the id returned ok return the JWT
                             //  with the id as a property
                             //  with the token as the value.
-                            return (object) $JWT->{$id} = $token;
+                            return new Token($id, $token_string);
                         }
                         else
                         {
@@ -190,6 +183,11 @@
                     return false;
                 }
             }
+            else
+            {
+                $this->error = 'Request to create token for already active user.';
+                return false;
+            }
         }
 
 		/*
@@ -208,52 +206,31 @@
             $this->error = '';
 
             //  Confirm the JWT.
-			if (!$this->isJWT($JWT)) { return false; }
-
-            //  If the JWT passed save the property name as the id.
-			$id = intval(key($JWT));
+			if (!$JWT instanceof Token) { return false; }
 
             //  Then get the entry information for that id.
-            $active_entry = $this->readActive($id);
+            $active_entry = $this->activeEntry($JWT->getId());
 
             //  Compare the token from the database to the given token.
-            if ($active_entry && $active_entry->token === $JWT->$id)
+            if ($active_entry && $active_entry->getToken() === $JWT->getToken())
             {
-                //  If the tokens match try to decode the one from the database.
-                $decoded_token;
-                try { $decoded_token = $this->JWT->decode(
-                    $active_entry, self::$key, true); }
-                catch (Exeption $e)
-                {
-                    //  If there was an error decoding the token
-                    //  log it and return false.
-                    $this->error = 'WARNING: Unable to decode JWT: '.$e->errno.' : '.$e;
-                    self::$logger->log(
-                        self::$logName,
-                        $this->error
-                    );
-                    return false;
-                }
-
                 //  If the decoding did not fail
                 //  check that the timestamp is from within 10 minuites.
                 //  (10*60sec)
-                if ($decoded_token->timestamp > time()-(10*60))
+                if ($active_entry->getTimestamp() > (time()-(10*60)))
                 {
-                    //  If the time had not expired
-                    //  update the timestamp and encode the token again.
-                    $decoded_token->timestamp = time();
-                    $new_token = $this->encode($decoded_token, self::$key);
+                    $decoded_token = $this->JWT->decode($active_entry->getToken(), self::$key, true);
+                    $new_token = $this->JWT->encode($decoded_token, self::$key);
+
+                    $active_entry->setToken($new_token);
+                    $active_entry->updateTimestamp();
 
                     //  Write the new token to the database.
-                    if ($this->updateToken($id, $new_token))
+                    if ($this->updateToken($active_entry->getId(), $active_entry->getToken()))
                     {
                         //  If the token was updated successfully
                         //  return the decoded token and the new encoded token.
-                        return (object)[
-                            'data' => $decoded_token,
-                            'new_JWT' => $new_token
-                        ];
+                        return $active_entry;
                     }
                     else
                     {
@@ -269,10 +246,13 @@
                     //  check if the call to this function was internal.
                     //  If not call delete else return true.
                     $this->error = 'Expired timestamp.';
-                    if (!$internal_call) { $this->delete($JWT); }
+                    if (!$internal_call)
+                    {
+                        //  Return false if the call was not intenal.
+                        $this->delete($JWT);
+                        return false;
+                    }
                     else { return true; }
-                    //  Return false if the call was not intenal.
-                    return false;
                 }
             }
             else
@@ -281,7 +261,7 @@
                 //  for the same id did not match
                 //  the token has been altered or is forged.
                 //  Log the error and return false.
-                $this->error = 'WARNING: Token missmatch! Suspected token forgery.';
+                $this->error = 'WARNING: Token mismatch! Suspected token forgery.';
                 self::$logger->log(
                     self::$logName,
                     $this->error
@@ -302,7 +282,7 @@
             $this->error = '';
 
             //  Confirm the JWT.
-            if (!$this->isJWT($JWT)) { return false; }
+            if (!$JWT instanceof Token) { return false; }
 
             //  Confirm the token is valid and belongs to an active user user.
             $valid = $this->confirm($JWT, true);
@@ -322,10 +302,10 @@
                 //  If the connection was successful
                 //  prepare the query to delete the entry.
                 if ($query = $mysql->connection->prepare(
-                    'DELETE * FROM ACTIVE_USERS WHERE ID = ? LIMIT 1'))
+                    'DELETE FROM ACTIVE_USERS WHERE ID = ? LIMIT 1'))
     			{
                     //  Bind the id as the parameter and run the query.
-    				$query->bind_param('i', key($JWT));
+    				$query->bind_param('i', $JWT->getId());
     				$query->execute();
 
                     //  Close the query and the connection.
@@ -334,7 +314,7 @@
 
                     //  Confirm that the query was successful
                     //  and return true if so, false if not.
-    				$result = $this->readActive(key($JWT));
+    				$result = $this->activeEntry($JWT->getId());
     				if (!$result) { return true; }
     				else { return false; }
     			}
@@ -360,81 +340,6 @@
                 return false;
             }
         }
-
-		/*
-		*   @description    This function confirms a username and hash
-        *                   combination matches a set in the users regestry.
-        *
-        *   @arguments      $username   : The name of the user.
-        *                   $password   : The password hash that should
-        *                                 belong to that user.
-        *   @returns        Boolean     : True if the set is matching,
-        *                                 false if not.
-		* */
-		private function confirmUser ($username, $password)
-		{
-            //  Reset the error string.
-            $this->error = '';
-
-            //  Confirm the username and password are non-empty strings.
-            if (!is_string($username) || $username == '' ||
-                !is_string($password) || $password == '')
-            {
-                $this->error = 'The username and password must be non-empty strings.';
-                return false;
-            }
-
-            //  If the username and password are non-empty strings
-            //  connect to the database.
-			$mysql = parent::connect();
-
-            //  Confirm the connection to the database.
-            if (!$mysql->error)
-            {
-                //  If the connection was successfull prepare the query
-                //  to fetch the hash of the passed username.
-                if ($query = $mysql->connection->prepare(
-                    'SELECT PASSWORD FROM USERS WHERE USERNAME = ?'))
-    			{
-                    //  Bind the username as the parameter and run the query.
-    				$query->bind_param('s', $username);
-    				$query->execute();
-
-                    //  Bind the result and fetch it.
-    				$query->bind_result($hash);
-    				$query->fetch();
-
-                    //  Close the query and the connection.
-    				$query->close();
-    				$mysql->connection->close();
-
-                    //  Confirm that the passed password matches
-                    //  the hash in the database. If so return true,
-                    //  if not return false.
-    				if ($password === $hash) { return true; }
-    				else { return false; }
-    			}
-                else
-                {
-                    //  If the query could not be prepared set the error
-                    //  before closing the database connection and return fasle.
-                    $this->error = 'Could not confirm user information: '.$mysql->connection->error;
-                    $mysql->connection->close();
-                    return false;
-                }
-            }
-            else
-            {
-                //  If the database connection was unsuccessful
-                //  log the error and return fasle.
-                $this->error = 'ERROR: Could not connect to the database: '.$mysql->connection->error;
-                self::$logger->log(
-                    self::$logName,
-                    $this->error
-                );
-                return false;
-            }
-		}
 
         /*
         *   @description    This function checks if a user
@@ -466,14 +371,14 @@
             {
                 //  If the connection was successful prepare the query.
                 if ($query = $mysql->connection->prepare(
-                    'SELECT ID FROM ACTIVE_USERS WHERE USERNAME = ?'))
+                    'SELECT ID, TOKEN, TIMESTAMP FROM ACTIVE_USERS WHERE USERNAME = ? LIMIT 1'))
                 {
                     //  Bind the username to the query and run it.
                     $query->bind_param('s', $username);
                     $query->execute();
 
                     //  Bind the result and fetch it.
-                    $query->bind_result($id);
+                    $query->bind_result($id, $token, $timestamp);
                     $query->fetch();
 
                     //  Close the query and the connection.
@@ -482,8 +387,16 @@
 
                     //  If the query returned no retult
                     //  return true for inactive, otherwise return false.
-                    if (!$id) { return true; }
-                    else { return false; }
+                    if ($id)
+                    {
+                        if (strtotime($timestamp) < (time()-(10*60))) {
+                            $JWT = new Token($id, $token);
+                            $this->delete($JWT);
+                            return true;
+                        }
+                        else { return false; }
+                    }
+                    else { return true; }
                 }
                 else
                 {
@@ -576,40 +489,12 @@
         }
 
         /*
-        *   @description    This function confirms that the argument
-        *                   follows the construct of a valid JWT.
-        *
-        *   @arguments      $JWT    : The token object to confirm.
-        *   @returns        Boolean : True if the $JWT has valid construct,
-        *                   false if not.
-        * */
-        private function isJWT($JWT)
-        {
-            //  Confirm that the JWT is an object with a numeric property
-            //  that contains a non-empty string.
-			if (!is_object($JWT) || !is_numeric(key($JWT)) ||
-                !is_string($JWT->{key($JWT)}) && $JWT->{key($JWT)} === '')
-			{
-                //  If the passed JWT did not match the specification
-                //  log the problem and return false.
-                $this->error = 'WARNING: The passed JWT did not match original construct.';
-				self::$logger->log(
-                    self::$logName,
-                    $this->error
-                );
-				return false;
-			}
-            //  Return true if it did not fail the check.
-            return true;
-        }
-
-        /*
 		*   @description    This function reads the information at a given id.
         *
         *   @arguments      $id : The id of the row to read.
         *   @returns        (array)['id', 'username', 'token']
 		* */
-		private function readActive ($id)
+		private function activeEntry ($id)
 		{
             //  Reset the error string.
             $this->error = '';
@@ -622,9 +507,6 @@
 				return false;
 			}
 
-            //  If the id was numeric cast it to an integer.
-			$id = intval($id);
-
             //  Connect to the database.
 			$mysql = parent::connect();
 
@@ -634,21 +516,23 @@
                 //  If the connection was successful
                 //  prepare the query to read the specified row.
                 if ($query = $mysql->connection->prepare(
-                    'SELECT * FROM ACTIVE_USERS WHERE ID = ?'))
+                    'SELECT ID, TOKEN, TIMESTAMP FROM ACTIVE_USERS WHERE ID = ? LIMIT 1'))
     			{
                     //  Bind the id as the paramater and run the query.
     				$query->bind_param('i', $id);
     				$query->execute();
 
                     //  Bind the result and fetch it.
-    				$query->bind_result($result);
+    				$query->bind_result($id, $token, $timestamp);
     				$query->fetch();
 
                     //  Close the query and the connection
                     //  before returning the result of the query.
     				$query->close();
     				$mysql->connection->close();
-    				return $result;
+
+                    if ($timestamp) { return new Token($id, $token, strtotime($timestamp)); }
+                    else { return false; }
     			}
                 else
                 {
@@ -679,13 +563,13 @@
         *                   $new_token  : The new token to input at
         *                                 the specified row.
         * */
-        private function updateToken($id, $new_token)
+        private function updateToken ($id, $new_token)
         {
             //  Reset the error string.
             $this->error = '';
 
             //  Confirm that the id and new token are valid values.
-            if (!is_numeric($id) || !is_string($new_token) && $new_token == '')
+            if (!is_numeric($id) || !is_string($new_token) && $new_token === '')
             {
                 $this->error = 'Unable to update token, passed id or token was invalid.';
                 return false;
@@ -698,8 +582,7 @@
             if (!$mysql->error)
             {
                 //  If connection was successful prepare thne query to update the token.
-                if ($query = $mysql->connection->pepare(
-                    'UPDATE ACTIVE_USERS SET TOKEN = ? WHERE ID = ?'))
+                if ($query = $mysql->connection->prepare('UPDATE ACTIVE_USERS SET TOKEN = ? WHERE ID = ?'))
                 {
                     //  Bind the new token and the id
                     //  to the query and run it.
@@ -712,11 +595,11 @@
 
                     //  Confirm that the query was successful
                     //  by reading the current value.
-                    $active_info = $this->readActive($id);
+                    $active_entry = $this->activeEntry($id);
 
                     //  If the current value matches the new token
                     //  return true, if not return false.
-                    if ($active_info->token == $new_token) { return true; }
+                    if ($active_entry->getToken() === $new_token) { return true; }
                     else { return false; }
                 }
                 else
