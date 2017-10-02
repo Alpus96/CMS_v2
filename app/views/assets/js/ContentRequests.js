c844aeaa18f9973d0257ae8938e3e65dac700e64
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-10-02 21:06:52 +0200

    Added class for edit requests of content

--- app/views/assets/js/ContentRequests.js
+++ app/views/assets/js/ContentRequests.js
@@ -9,17 +9,29 @@
 class ContentRequests {
     constructor () { this.error = ''; }
 
-    create (content) {
+    create (type, content) {
+        //  TODO: Confirm content is valid.
+        const url = '/create'+type;
+        AJAX.post(url, content, (err, res) => {
 
+        });
     }
 
-    update (id, content) {
+    update (type, content) {
+        //  TODO: Confirm content is valid. (+has id)
+        const url = '/update'+type;
+        AJAX.post(url, content, (err, res) => {
 
+        });
     }
 
-    delete (id) {
+    delete (type, id) {
+        //  TODO: Confirm id is valid.
+        const url = '/delete'+type;
+        AJAX.post(url, id, (err, res) => {
 
+        });
     }
 }
 
-const contentRequest = new contentRequests();
+const contentRequest = new ContentRequests();
