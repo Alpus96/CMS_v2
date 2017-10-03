da9a2df23a438ef6d0a34ef4e6c714a6108e6651
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-10-03 23:58:38 +0200

    Moved method to top of file

--- app/views/assets/CMS/js/contentContainer.js
+++ app/views/assets/CMS/js/contentContainer.js
@@ -11,6 +11,27 @@ class ContentContainer {
         this.noResponseError = '<p class="alert alert-danger">Kunde inte hämta data!</p>';
     }
 
+    async loadContent () {
+        const loader = new ContentLoader();
+        let content = await loader.read(this.contentType, this.category, this.amount);
+        if (content) {
+            if (content.substr(0,1) === '{') {
+                content = JSON.parse(content);
+                const entryType = this.contentType.charAt(0).toUpperCase() + this.contentType.slice(1) + 'Entry';
+                for (let key in content) {
+                    let entry = content[key];
+                    const entryObj = new ContentEntry(this.id, this.contentType, this.category, entry.id, entry.content, entry.timestamp, entry.edited);
+                    this.contents.push(entryObj);
+                }
+            } else {
+                $('#'+this.id).html(content);
+            }
+        } else {
+            //  NOTE:  Show error or no data message?
+            $('#'+this.id).html(this.noResponseError);
+        }
+    }
+
     updateType (newType) {
         if (typeof newType != 'string' && newType.indexOf('') != -1)
         { return false; }
@@ -36,33 +57,6 @@ class ContentContainer {
         $('#'+id).removeClass(this.amount);
         $('#'+id).addClass(newAmount);
         this.contentType = newAmount;
-    }
-
-    async loadContent () {
-        const loader = new ContentLoader();
-        let content = await loader.read(this.contentType, this.category, this.amount);
-        if (content) {
-            if (content.substr(0,1) === '{') {
-                content = JSON.parse(content);
-                const entryType = this.contentType.charAt(0).toUpperCase() + this.contentType.slice(1) + 'Entry';
-                for (let key in content) {
-                    let entry = content[key];
-                    //  Create instances of entry.
-                    const entryObj = new ContentEntry(this.contentType, this.category, entry.id, entry.content, entry.timestamp, entry.edited);
-                    this.contents.push(entryObj);
-
-                    $('#'+this.id).append(entryObj.toHTML());
-                    if (cookie.read('token')) {
-                        this.contents[this.contents.length-1].addEditListners();
-                    }
-                }
-            } else {
-                $('#'+this.id).html(content);
-            }
-        } else {
-            //  NOTE:  Show error or no data message?
-            $('#'+this.id).html(this.noResponseError);
-        }
     }
 
 }
