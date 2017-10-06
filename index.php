2de4b11a01cd0f1c116b67642630dca086a64702
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-10-06 22:17:06 +0200

    implemented new request handler

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     *           with '/CMS' before integration.
     * */
     $url = str_replace('/projects/CMS_v2', '', $_SERVER['REQUEST_URI']);
-    require_once 'app/controllers/routes.php';
-    $routes = new Routes();
+    require_once 'app/ResponseHandler.php';
+    $responseHandler = new ResponseHandler($url);
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') { $routes->get($url); }
-    else if ($_SERVER['REQUEST_METHOD'] === 'POST') { $routes->post($url); }
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') { $responseHandler->get(); }
+    else if ($_SERVER['REQUEST_METHOD'] === 'POST') { $responseHandler->post(); }
  ?>
