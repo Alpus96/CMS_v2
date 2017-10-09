0a23466723dd74061f7e769172ce600cd7c3a802
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-10-09 15:08:54 +0200

    Fixed modal dismiss issue where remove was never called on non-action

--- app/views/assets/lib/msgHelper.js
+++ app/views/assets/lib/msgHelper.js
@@ -51,11 +51,14 @@ class MsgHelper {
 
     newModal (title, content, options) {
         if (!this.selector) { console.warn('jQuery undefined!'); return; }
-        //this.removeModal();
+        
         const modal = '<div class="msgHelperModal modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">'+title+'</h4></div><div class="modal-body">'+content+'</div><div class="modal-footer">'+options+'</div></div></div></div>';
         $('body').append(modal);
+
         $('.msgHelperModal').modal({ show: false });
         $('.msgHelperModal').modal('show');
+
+        $('.msgHelperModal').on('hidden.bs.modal', () => { this.removeModal(); });
     }
 
     removeModal () {
